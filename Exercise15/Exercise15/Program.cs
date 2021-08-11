4012beac281873396faf98f5358fa35130788a36
Maronda Anderson-Johnson <66698985+Maronda12@users.noreply.github.com>
2021-08-11 16:03:21 -0400

    Add files via upload

---
+++ Exercise15/Exercise15/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+            while (goOn == true)
+            {
+
+                Console.WriteLine("Please enter a number");
+                Console.WriteLine("-------------------");
+
+                double number = double.Parse(Console.ReadLine());
+
+                int counter;
+                double maxValue = number;
+
+                //Cubes & Prints
+                for (counter = 1; counter <= maxValue; counter++)
+                {
+                    int cubed = counter * counter * counter;
+                    Console.WriteLine("{0}   {1}", counter, cubed);
+                }
+
+                //Continue Loop
+                while (true)
+                {
+                    Console.WriteLine($"Would you like to continue? yes/no");
+                    string answer = Console.ReadLine();
+
+                    if (answer.ToLower() == "yes")
+                    {
+                        goOn = true;
+                        break;
+                    }
+                    else if (answer.ToLower() == "no")
+                    {
+                        Console.WriteLine("Good Bye!");
+                        goOn = false;
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Im sorry I didnt understand that lets try again");
+                    }
+                }
+
+            }
+        }
+    }
+}
+    
+
